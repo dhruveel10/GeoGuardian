7277e35f112d81320c0f055e4fd7cee5bfc0a1e6
dhruveel10 <dhruveel.chouhan.3@gmail.com>
2025-09-07 18:01:27 +0530

    added for cors for vercel

--- geoguardian-api/src/server.ts
+++ geoguardian-api/src/server.ts
@@ -18,7 +18,14 @@ app.use(helmet({
   contentSecurityPolicy: false 
 }));
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://localhost:5173', 'http://127.0.0.1:5173'],
+  origin: [
+    'http://localhost:3000', 
+    'http://127.0.0.1:3000', 
+    'http://localhost:5173', 
+    'http://127.0.0.1:5173',
+    'https://geo-guardian-99dyxisjy-dhruveel10s-projects.vercel.app',
+    /https:\/\/.*\.vercel\.app$/
+  ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Access-Control-Allow-Origin'],
   credentials: true
